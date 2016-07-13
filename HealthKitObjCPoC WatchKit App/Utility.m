e26c158f223bded248dbd7e5a6402529c6cafaa2
Sean Petykowski <spetykowski@gmail.com>
2016-07-12 23:01:49 -0400

    -- Update utility class

---
+++ HealthKitObjCPoC WatchKit App/Utility.m
@@ -0,0 +1,33 @@
+//
+//  Utility.m
+//  HealthKitObjCPoC
+//
+//  Created by Sean Petykowski on 7/12/16.
+//  Copyright © 2016 Sean Petykowski. All rights reserved.
+//
+
+#import "Utility.h"
+
+@implementation Utility
+
++(NSDateFormatter*)dateFormatterForTimeLabels {
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateStyle = NSDateFormatterNoStyle;
+    dateFormatter.timeStyle = NSDateFormatterShortStyle;
+    
+    return dateFormatter;
+    
+}
+
++(NSDictionary*)contentsOfHealthPlist {
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"Health.plist"];
+    NSDictionary *plistDictionary = [NSDictionary dictionaryWithContentsOfFile:filePath];
+    
+    return plistDictionary;
+}
+
+@end
