b5329c17c8515b9e953803d85a95f1635dc62029
Sean Petykowski <spetykowski@gmail.com>
2016-11-20 17:45:34 -0500

    Chart Will Plot Heart Rate Data
    Continued development on #40
    
    This commit will plot heart rate data on the chart. The chart will also display a line which indicates the average heart rate value.
    
    * Disabled x-axis labels temporarily
    * Added initWithDouble to SleepStatistic class

--- HealthKitObjCPoC/ChartViewController.m
+++ HealthKitObjCPoC/ChartViewController.m
@@ -16,17 +16,21 @@
 @property (strong, nonatomic) IBOutlet UILabel *chartLabel;
 @property (strong, nonatomic) IBOutlet UIStackView *yAxisStack;
 @property (strong, nonatomic) IBOutlet UIStackView *xAxisStack;
+@property SleepStatistic *minStatistic;
+@property SleepStatistic *maxStatistic;
+@property SleepStatistic *avgStatistic;
+@property SleepStatistic *sleepDuration;
 @property SleepSession *detailSleepSession;
+@property NSNumber *chartMax;
+@property NSNumber *chartMin;
 @property NSArray *ktimes12Hour;
 @property NSMutableArray *heartRateMilestones;
-
 @end
 
 @implementation ChartViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     _heartRateMilestones = [[NSMutableArray alloc] init];
     
     self.view.backgroundColor = [UIColor colorWithRed:0.1137254902 green:0.1137254902 blue:0.1137254902 alpha:1.0];
@@ -36,7 +40,7 @@
     [_chartLabel setText:_chartTitle];
     [_chartLabel setTextColor:[UIColor whiteColor]];
     [self refreshHealthStatistics];
-    [self drawSeperatorLines];
+    [self drawChartSeperatorLines];
     
 }
 
@@ -45,7 +49,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)drawSeperatorLines {
+- (void)drawChartSeperatorLines {
     NSArray *seperatorLocations = @[@35, @240];
     
     UIView *verticalLine=[[UIView alloc]initWithFrame:CGRectMake(45, 35, 1, 205)];
@@ -61,45 +65,45 @@
 }
 
 - (void)updateAxisLabels {
-    int indexCount = 0;
-    int maxNumberOfLabels = 6;
-    NSArray *xAxisLabelsFromSleepSession = [self generateXAxisLabels];
-    NSMutableArray *xAxisLabelsToDisplay = [[NSMutableArray alloc] init];
+//    int indexCount = 0;
+//    int maxNumberOfLabels = 6;
+//    NSArray *xAxisLabelsFromSleepSession = [self generateXAxisLabels];
+//    NSMutableArray *xAxisLabelsToDisplay = [[NSMutableArray alloc] init];
     
 //    xAxisLabelsFromSleepSession = @[@"12 AM", @"1 AM", @"2 AM", @"3 AM", @"4 AM", @"5 AM", @"6 AM", @"7 AM", @"8 AM"];
     
-    if (xAxisLabelsFromSleepSession.count < maxNumberOfLabels) {
-        
-        maxNumberOfLabels = (int)xAxisLabelsFromSleepSession.count;
-        [xAxisLabelsToDisplay addObjectsFromArray:xAxisLabelsFromSleepSession];
-        
-    } else if (xAxisLabelsFromSleepSession.count <= 11) {
-        
-        while (xAxisLabelsToDisplay.count <= 6 && indexCount <= xAxisLabelsFromSleepSession.count) {
-            [xAxisLabelsToDisplay addObject:xAxisLabelsFromSleepSession[indexCount]];
-            indexCount += 2;
-        };
-    } else if (xAxisLabelsFromSleepSession.count > 11) {
-        
-        for (int x = 0; x < maxNumberOfLabels; x++) {
-            if (x == 0) {
-                [xAxisLabelsToDisplay addObject:[xAxisLabelsFromSleepSession firstObject]];
-            } else if (x == maxNumberOfLabels -1) {
-                [xAxisLabelsToDisplay addObject:[xAxisLabelsFromSleepSession lastObject]];
-            } else {
-                indexCount += 2;
-                [xAxisLabelsToDisplay addObject:xAxisLabelsFromSleepSession[indexCount]];
-            }
-        }
-    }
-    
-    for (int x = 0; x < xAxisLabelsToDisplay.count; x++) {
-        UILabel *xAxisLabel = [[UILabel alloc] init];
-        [xAxisLabel setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleCaption1]];
-        [xAxisLabel setTextColor:[UIColor whiteColor]];
-        [xAxisLabel setText:xAxisLabelsToDisplay[x]];
-        [_xAxisStack addArrangedSubview:xAxisLabel];
-    }
+//    if (xAxisLabelsFromSleepSession.count < maxNumberOfLabels) {
+//        
+//        maxNumberOfLabels = (int)xAxisLabelsFromSleepSession.count;
+//        [xAxisLabelsToDisplay addObjectsFromArray:xAxisLabelsFromSleepSession];
+//        
+//    } else if (xAxisLabelsFromSleepSession.count <= 11) {
+//        
+//        while (xAxisLabelsToDisplay.count <= 6 && indexCount <= xAxisLabelsFromSleepSession.count) {
+//            [xAxisLabelsToDisplay addObject:xAxisLabelsFromSleepSession[indexCount]];
+//            indexCount += 2;
+//        };
+//    } else if (xAxisLabelsFromSleepSession.count > 11) {
+//        
+//        for (int x = 0; x < maxNumberOfLabels; x++) {
+//            if (x == 0) {
+//                [xAxisLabelsToDisplay addObject:[xAxisLabelsFromSleepSession firstObject]];
+//            } else if (x == maxNumberOfLabels -1) {
+//                [xAxisLabelsToDisplay addObject:[xAxisLabelsFromSleepSession lastObject]];
+//            } else {
+//                indexCount += 2;
+//                [xAxisLabelsToDisplay addObject:xAxisLabelsFromSleepSession[indexCount]];
+//            }
+//        }
+//    }
+//    
+//    for (int x = 0; x < xAxisLabelsToDisplay.count; x++) {
+//        UILabel *xAxisLabel = [[UILabel alloc] init];
+//        [xAxisLabel setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleCaption1]];
+//        [xAxisLabel setTextColor:[UIColor whiteColor]];
+//        [xAxisLabel setText:xAxisLabelsToDisplay[x]];
+//        [_xAxisStack addArrangedSubview:xAxisLabel];
+//    }
     
     for (int x = 0; x < _heartRateMilestones.count; x++) {
         UILabel *yAxisLabel = [[UILabel alloc] init];
@@ -130,7 +134,9 @@
     }
     
     // Makes the Array count odd to allow for displaying every other item
-    if (durationHours % 2 && durationHours > 6) {
+    if (durationHours % 2) {
+        // durationHours is odd... do nothing
+    } else if (durationHours > 6) {
         durationHours++;
     }
     
@@ -148,12 +154,15 @@
 
 - (NSPredicate *)predicateForSleepDuration {
     
-    return [HKQuery predicateForSamplesWithStartDate:[_detailSleepSession.sleep firstObject] endDate:[_detailSleepSession.wake lastObject] options:HKQueryOptionNone];
+    return [HKQuery predicateForSamplesWithStartDate:[_detailSleepSession.inBed firstObject] endDate:[_detailSleepSession.outBed lastObject] options:HKQueryOptionNone];
 }
 
 - (void)refreshHealthStatistics {
     [self generateYAxisLabels:^(NSError *error) {
-        [self updateAxisLabels];
+        [self plotHeartRateOnGraph:^(NSError *error){
+            [self updateAxisLabels];
+            [self drawAverageHeartRateLine];
+        }];
     }];
 }
 
@@ -175,17 +184,22 @@
                 
                 if (result) {
                     
-                    SleepStatistic *minStatistic = [[SleepStatistic alloc] init];
-                    SleepStatistic *maxStatistic = [[SleepStatistic alloc] init];
-                    
-                    minStatistic.result = [result.minimumQuantity doubleValueForUnit:[HKUnit unitFromString:@"count/min"]];
-                    maxStatistic.result = [result.maximumQuantity doubleValueForUnit:[HKUnit unitFromString:@"count/min"]];
+                    _minStatistic = [[SleepStatistic alloc] initWithDouble:[result.minimumQuantity doubleValueForUnit:[HKUnit unitFromString:@"count/min"]]];
+                    _maxStatistic = [[SleepStatistic alloc] initWithDouble:[result.maximumQuantity doubleValueForUnit:[HKUnit unitFromString:@"count/min"]]];
+                    _avgStatistic = [[SleepStatistic alloc] initWithDouble:[result.averageQuantity doubleValueForUnit:[HKUnit unitFromString:@"count/min"]]];
             
-                    minStatistic.stringResult = [NSString stringWithFormat:@"%@ bpm", [numberFormatterDown stringFromNumber:[NSNumber numberWithFloat:minStatistic.result]]];
-                    maxStatistic.stringResult = [NSString stringWithFormat:@"%@ bpm", [numberFormatterUp stringFromNumber:[NSNumber numberWithFloat:maxStatistic.result]]];
+                    _minStatistic.stringResult = [NSString stringWithFormat:@"%@ bpm", [numberFormatterDown stringFromNumber:[NSNumber numberWithFloat:_minStatistic.result]]];
+                    _maxStatistic.stringResult = [NSString stringWithFormat:@"%@ bpm", [numberFormatterUp stringFromNumber:[NSNumber numberWithFloat:_maxStatistic.result]]];
+                    _avgStatistic.stringResult = [NSString stringWithFormat:@"%@ bpm", [numberFormatterUp stringFromNumber:[NSNumber numberWithFloat:_avgStatistic.result]]];
                     
-                    [_heartRateMilestones addObject:maxStatistic];
-                    [_heartRateMilestones addObject:minStatistic];
+                    NSString *min = [NSString stringWithFormat:@"%@", [numberFormatterDown stringFromNumber:[NSNumber numberWithFloat:_minStatistic.result]]];
+                    NSString *max = [NSString stringWithFormat:@"%@", [numberFormatterUp stringFromNumber:[NSNumber numberWithFloat:_maxStatistic.result]]];
+                    
+                    _chartMin = [numberFormatterDown numberFromString:min];
+                    _chartMax = [numberFormatterUp numberFromString:max];
+                    
+                    [_heartRateMilestones addObject:_maxStatistic];
+                    [_heartRateMilestones addObject:_minStatistic];
                 }
                 if (completionHandler) {
                     completionHandler(error);
@@ -194,6 +208,87 @@
         }];
         
     [self.healthStore executeQuery:maxHeartRateQuery];
+}
+
+-(void) plotHeartRateOnGraph:(void (^)(NSError *))completionHandler {
+    // Testing
+    double chartFrameWidth = self.view.frame.size.width - 45;
+    double chartFrameHeight = 205;
+    CGPoint chartOrigin = CGPointMake(45, 36);
+    int dataPointRadius = 1;
+    double heartRateRange = _chartMax.doubleValue - _chartMin.doubleValue;
+    
+    
+    // Calculate Total Time Asleep
+    NSDateComponents *totalSessionDurationComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitSecond fromDate:[_detailSleepSession.inBed firstObject] toDate:[_detailSleepSession.outBed lastObject] options:0];
+    NSInteger hours = [totalSessionDurationComponents hour];
+    NSInteger minutes = [totalSessionDurationComponents minute];
+    NSInteger seconds = [totalSessionDurationComponents second];
+    
+    double totalSleepDuration = (hours * 3600) + (minutes * 60) + seconds;
+    // End Testing
+    
+    NSDate *sampleStartDate = [_detailSleepSession.inBed firstObject];
+    NSDate *sampleEndDate = [_detailSleepSession.outBed lastObject];
+    
+    HKSampleType *sampleType = [HKSampleType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRate];
+    NSPredicate *predicate = [HKQuery predicateForSamplesWithStartDate:sampleStartDate endDate:sampleEndDate options:HKQueryOptionNone];
+    
+    HKSampleQuery *query = [[HKSampleQuery alloc] initWithSampleType:sampleType predicate:predicate limit:HKObjectQueryNoLimit sortDescriptors:nil resultsHandler:^(HKSampleQuery * _Nonnull query, NSArray<__kindof HKSample *> * _Nullable results, NSError * _Nullable error) {
+        if (!results) {
+            NSLog(@"An error has occured. The error was: %@", error);
+            abort();
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            for (HKQuantitySample *sample in results) {
+                NSDateComponents *durationToReading = [[NSCalendar currentCalendar] components:NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitSecond fromDate:[_detailSleepSession.inBed firstObject] toDate:sample.startDate options:0];
+                NSInteger sampleHours = [durationToReading hour];
+                NSInteger sampleminutes = [durationToReading minute];
+                NSInteger sampleseconds = [durationToReading second];
+                
+                // Calculate X Coordinate
+                double sampleDuration = (sampleHours * 3600) + (sampleminutes * 60) + sampleseconds;
+                double xCoordinate = (sampleDuration * chartFrameWidth) / totalSleepDuration;
+                
+                // Calculate Y Coordinate
+                double heartRate = [sample.quantity doubleValueForUnit:[HKUnit unitFromString:@"count/min"]];
+                double adjustedRate = _chartMax.doubleValue - heartRate;
+                double yCoordinate = (adjustedRate * chartFrameHeight) / heartRateRange;
+                
+                UIBezierPath *circle = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(xCoordinate + 45 - dataPointRadius, chartOrigin.y + yCoordinate - dataPointRadius, dataPointRadius * 2, dataPointRadius * 2)];
+                CAShapeLayer *fillLayer = [CAShapeLayer layer];
+                fillLayer.frame = CGRectMake(xCoordinate + 45, 100, dataPointRadius, dataPointRadius);
+                fillLayer.bounds = CGRectMake(xCoordinate + 45, 100, dataPointRadius, dataPointRadius);
+                fillLayer.path = circle.CGPath;
+                fillLayer.strokeColor = [UIColor whiteColor].CGColor;
+                fillLayer.fillColor = [UIColor whiteColor].CGColor;
+                fillLayer.lineWidth = 1;
+                fillLayer.lineJoin = kCALineJoinRound;
+                [self.view.layer addSublayer:fillLayer];
+                
+            }
+            if (completionHandler) {
+                completionHandler(error);
+            }
+        });
+    }];
+    
+    [self.healthStore executeQuery:query];
+}
+
+-(void)drawAverageHeartRateLine {
+    CGPoint chartOrigin = CGPointMake(45, 36);
+    double chartFrameHeight = 205;
+    if (_avgStatistic.result) {
+        double heartRateRange = _chartMax.doubleValue - _chartMin.doubleValue;
+        double adjustedRate = _chartMax.doubleValue - _avgStatistic.result;
+        double yCoordinate = (adjustedRate * chartFrameHeight) / heartRateRange;
+        UIView *horizontalLine = [[UIView alloc]initWithFrame:CGRectMake(45, chartOrigin.y + yCoordinate, self.view.bounds.size.width*2, 1)];
+        [horizontalLine setBackgroundColor:[UIColor colorWithRed:0.3725490196 green:0.3058823529 blue:0.7176470588 alpha:1]];
+        [self.view addSubview:horizontalLine];
+    }
 }
 
 @end
